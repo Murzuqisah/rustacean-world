c4f0b7e3b763c23019ded71773671ae971e9aded
jamos <amosjoel91@gmail.com>
2025-04-01 15:27:04 +0300

    Add celsius_to_fahrenheit function for temperature conversion

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -2,6 +2,10 @@ pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     (f - 32.0) * (5.0/ 9.0)
 }
 
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * (9.0 / 5.0) + 32.0
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
