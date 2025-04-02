e38fa7d8802adb41b5b5dff3ddc1a961789f5ed3
jamos <amosjoel91@gmail.com>
2025-04-02 12:17:27 +0300

    fix: remove duplicate celsius_to_fahrenheit function and improve formatting

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,8 @@
-pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f -32.0) / (9.0 / 5.0)
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * (9.0 / 5.0) + 32.0
 }
 
-pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c * (9 as f64 / 5 as f64)) + 32 as f64
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) / (9.0 / 5.0)
 }
+
