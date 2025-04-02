e99508ba5a88599c333ce969df18d3a1eb91f370
jamos <amosjoel91@gmail.com>
2025-04-02 06:11:42 +0300

    fix: fix return type to cap overflow wrapping

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32 as f64) * (5 as f64/ 9 as f64)
+    (f - 32 as f64) * 0.5555555555555555
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
