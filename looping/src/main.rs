1633f33349148fc2ff97b9b369df79bbb3d828fe
jamos <amosjoel91@gmail.com>
2025-04-02 14:40:17 +0300

    refactor: refine function body to minimize references

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,21 +1,23 @@
 use std::io;
 
 fn main() {
-    println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let answer = "The letter e";
-    let mut tries = 1;
+    let mut tries = 0;
 
+    println!("{}", riddle);
     
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("failed to read line");
+        tries += 1;
+
         if answer == input.trim() {
             println!("Number of trials: {}", tries);
             break;
         } else {
-            println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+            println!("{}", riddle);
         }
-        tries += 1;
     }
 
 }
