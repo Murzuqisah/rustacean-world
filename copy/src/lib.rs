92bc772a805ff5334924c4a6f2dc9dd64847206a
jamos <amosjoel91@gmail.com>
2025-04-03 19:25:36 +0300

    fix: remove imports

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,27 +1,20 @@
-use std::f64::consts::E;
-use std::f64::INFINITY;
-use std::f64::NEG_INFINITY;
-use std::f64::consts::LN_2;
-use std::f64::consts::LN_10;
-
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     let exp = (c as f64).exp();
-    let ln = if c == 0{
-        NEG_INFINITY
+    let ln = if c == 0 {
+        f64::NEG_INFINITY // log(0) is negative infinity
     } else {
-        c.abs() as f64
+        (c.abs() as f64).ln()
     };
-
-    return (c, exp, ln)
+    (c, exp, ln)
 }
 
 pub fn str_function(a: String) -> (String, String) {
-    let exp_values: Vec<String> = a
-        .split_whitespace()
-        .map(|s| {
-            let number: f64 = s.parse().unwrap();
-            format!("{}", number * number)
-        })
+    let numbers: Vec<f64> = a.split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap() as f64) // Convert each part to f64
+        .collect();
+
+    let exp_values: Vec<String> = numbers.iter()
+        .map(|&n| n.exp().to_string())
         .collect();
 
     (a, exp_values.join(" "))
@@ -29,7 +22,13 @@ pub fn str_function(a: String) -> (String, String) {
 
 pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
     let ln_values: Vec<f64> = b.iter()
-        .map(|&n| (n.abs() as f64).ln())
+        .map(|&n| {
+            if n == 0 {
+                f64::NEG_INFINITY // log(0) is negative infinity
+            } else {
+                (n.abs() as f64).ln()
+            }
+        })
         .collect();
 
     (b, ln_values)
