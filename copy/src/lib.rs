7ce2d065f9d5645b000fe81f3061ba8778dd701f
jamos <amosjoel91@gmail.com>
2025-04-03 19:21:46 +0300

    fix: fix error in Vec and implementation

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,16 +1,27 @@
+use std::f64::consts::E;
+use std::f64::INFINITY;
+use std::f64::NEG_INFINITY;
+use std::f64::consts::LN_2;
+use std::f64::consts::LN_10;
+
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     let exp = (c as f64).exp();
-    let ln = (c.abs() as f64).ln()
-    (c, exp, ln)
+    let ln = if c == 0{
+        NEG_INFINITY
+    } else {
+        c.abs() as f64
+    };
+
+    return (c, exp, ln)
 }
 
 pub fn str_function(a: String) -> (String, String) {
-    let numbers: Vec<f64> = a.split_whitespace()
-        .map(|s| s.parse::<i32>().unwrap() as f64)
-        .collect();
-
-    let exp_values: vec<String> = numbers.iter()
-        .map(|&n| n.exp().to_string())
+    let exp_values: Vec<String> = a
+        .split_whitespace()
+        .map(|s| {
+            let number: f64 = s.parse().unwrap();
+            format!("{}", number * number)
+        })
         .collect();
 
     (a, exp_values.join(" "))
