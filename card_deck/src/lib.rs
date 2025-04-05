f23807be06799237341f0b4e9114688ab3d6f0b9
jamos <amosjoel91@gmail.com>
2025-04-05 08:28:33 +0300

    fix: implement clone on the card

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -75,6 +75,6 @@ pub struct Card {
 }
 
 pub fn winner_card(card: Card) -> bool {
-    let cloned_card: &Card = &card;
+    let cloned_card = card.clone();
     matches!(cloned_card.rank, Rank::Ace) && matches!(cloned_card.suit, Suit::Spade)
 }
