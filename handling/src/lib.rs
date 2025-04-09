ef3cd4ddcedce6decbeb7b633ec67c430efd3548
jamos <amosjoel91@gmail.com>
2025-04-09 11:14:45 +0300

    feat: open file if exists

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,16 +1,10 @@
 use std::path::Path;
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{Write};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    // if file exists, open it, otherwise create it
-    if path.as_ref().exists() {
-        return;
-    }
-    
     let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
+        .append(true)
         .create(true)
         .open(path)
         .expect("Failed to open or create file");
