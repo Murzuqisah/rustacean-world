809ac4ff5f26b32730cb2f1baf6d8840f2d3677a
jamos <amosjoel91@gmail.com>
2025-04-09 10:48:03 +0300

    feat: enhance open_or_create to open and write to files

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,7 +1,14 @@
 use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    if !path.as_ref().exists() {
-        std::fs::write(path, content).unwrap();
-    }
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)
+        .expect("Failed to open or create file");
+
+    file.write_all(content.as_bytes()).expect("Failed to write to file");    
 }
