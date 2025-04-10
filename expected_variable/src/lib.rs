64b25c0d391a17d573f83649a1ea6615430f8d9f
jamos <amosjoel91@gmail.com>
2025-04-11 00:24:13 +0300

    fix: correct dependency version

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,5 +1,33 @@
+extern crate case;
+
 use case::CaseExt;
-use edit_distance::edit_distance;
+
+pub fn edit_distance(s1: &str, s2: &str) -> usize {
+    let len1 = s1.chars().count();
+    let len2 = s2.chars().count();
+    let mut dp = vec![vec![0; len2 + 1]; len1 + 1];
+
+    for i in 0..=len1 {
+        dp[i][0] = i;
+    }
+    for j in 0..=len2 {
+        dp[0][j] = j;
+    }
+
+    for i in 1..=len1 {
+        for j in 1..=len2 {
+            let cost = if s1.chars().nth(i - 1) == s2.chars().nth(j - 1) {
+                0
+            } else {
+                1
+            };
+            dp[i][j] = (dp[i - 1][j] + 1)
+                .min(dp[i][j - 1] + 1)
+                .min(dp[i - 1][j - 1] + cost);
+        }
+    }
+    dp[len1][len2]
+}
 
 pub fn expected_variable(compare: &str, expected: &str) -> Option<String> {
     let compare_lower = compare.to_lowercase();
