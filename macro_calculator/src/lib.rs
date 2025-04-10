d7b86371ac2185fd79f1a41d1ed457c075933f34
jamos <amosjoel91@gmail.com>
2025-04-10 21:30:24 +0300

    fix: fix rounding off of values and ordering

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,40 +1,53 @@
-extern crate json;
+use json::{object, JsonValue};
 
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+#[derive(Debug, Clone)]
 pub struct Food {
-    pub name: String,
-    pub calories: [String; 2],
-    pub fats: f64,
-    pub carbs: f64,
-    pub proteins: f64,
-    pub nbr_of_portions: f64,
+    pub name:String,
+    pub calories:[String;2],
+    pub proteins:f64,
+    pub fats:f64,
+    pub carbs:f64,
+    pub nbr_of_portions:f64,
 }
 
-pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
-    let mut calories = 0.0;
-    let mut fats = 0.0; 
-    let mut carbs = 0.0;
-    let mut proteins = 0.0;
+fn round_value(value: f64) -> f64 {
+    let rounded = (value * 100.0).round() / 100.0;
+    if (rounded * 10.0) % 1.0 == 0.0 {
+        ((rounded * 10.0).round()) / 10.0
+    } else {
+        rounded
+    }
+}
+fn extract_number(s: &str) -> Option<f64> {
+    let number: String = s.chars().filter(|c| c.is_digit(10) || *c == '.').collect();
+    number.parse::<f64>().ok()
+}
+
+
+pub fn calculate_macros(foods: Vec<Food>) -> JsonValue {
+    let mut total_cals: f64 = 0.0;
+    let mut total_fats: f64 = 0.0;
+    let mut total_carbs: f64 = 0.0;
+    let mut total_proteins: f64 = 0.0;
 
     for food in foods {
-        let kcal: f64 = food.calories[1]
-            .trim_end_matches("kcal")
-            .parse()
-            .unwrap_or(0.0);
-
-
-        calories += kcal * food.nbr_of_portions;
-        fats += food.fats * food.nbr_of_portions;
-        carbs += food.carbs * food.nbr_of_portions;
-        proteins += food.proteins * food.nbr_of_portions;
+        if let Some(kcal) = extract_number(&food.calories[1]) {
+            let multiplier = food.nbr_of_portions;
+            total_cals += kcal * multiplier;
+            total_fats += food.fats * multiplier;
+            total_carbs += food.carbs * multiplier;
+            total_proteins += food.proteins * multiplier;
+        }
     }
-
-    let round_value = |value: f64| (value * 100.0).round() / 100.0;
-
     
-    json::object! {
-        "calories" => round_value(calories),
-        "carbs" => round_value(carbs),
-        "proteins" => round_value(proteins),
-        "fats" => round_value(fats),
+
+    object! {
+        "cals"    => round_value(total_cals),
+        "carbs"   => round_value(total_carbs),
+        "proteins"=> round_value(total_proteins),
+        "fats"    => round_value(total_fats)
     }
 }
