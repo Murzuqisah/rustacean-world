ca1d613c7c87f9e3b45c9855b313e2fc55df40dc
jamos <amosjoel91@gmail.com>
2025-04-16 07:18:19 +0300

    fix: export functions publicaly

--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -1,45 +1,41 @@
-use std::rc::Rc;
-
 pub trait Logger {
     fn warning(&self, msg: &str);
     fn info(&self, msg: &str);
     fn error(&self, msg: &str);
 }
 
-pub struct Tracker<'a, T: Logger> {
-    logger: &'a T,
+pub struct Tracker<'a> {
+    logger: &'a dyn Logger,
     value: usize,
     max: usize,
 }
 
-impl<'a, T: Logger> Tracker<'a, T> {
-    pub fn new(logger: &'a T, max: usize) -> Self {
-        Self {
+impl<'a> Tracker<'a> {
+    pub fn new(logger: &'a dyn Logger, max: usize) -> Self {
+        Tracker {
             logger,
             value: 0,
             max,
         }
     }
 
-    pub fn set_value(&self, rc: &Rc<u8>) {
-        let count = Rc::strong_count(rc);
-        let percent = (count * 100) / self.max;
+    pub fn set_value(&mut self, _track_value: &std::rc::Rc<usize>) {
+        self.value += 1;
 
-        if percent >= 100 {
-            self.logger.error("Error: you are over your quota!");
-        } else if percent >= 70 {
+        let percentage = (self.value * 100) / self.max;
+
+        if percentage >= 100 {
+            self.logger.error("you are over your quota!");
+        } else if percentage >= 70 {
             self.logger.warning(&format!(
-                "Warning: you have used up over {}% of your quota! Proceeds with precaution",
-                percent
+                "you have used up over {}% of your quota! Proceeds with precaution",
+                percentage
             ));
         }
     }
 
-    pub fn peek(&self, rc: &Rc<u8>) {
-        let count = Rc::strong_count(rc);
-        let percent = (count * 100) / self.max;
-
-        self.logger
-            .info(&format!("Info: you are using up to {}% of your quota", percent));
+    pub fn peek(&self, _track_value: &std::rc::Rc<usize>) {
+        let percentage = (self.value * 100) / self.max;
+        self.logger.info(&format!("you are using up to {}% of your quota", percentage));
     }
 }
