7f80f516ac995dda817cc2fc64e9b617efa4ee41
jamos <amosjoel91@gmail.com>
2025-04-18 16:11:14 +0300

    feat: new updates

---
+++ next-prime/src/main.rs
@@ -0,0 +1,5 @@
+use next_prime::*;
+
+fn main() {
+    println!("The next prime number is {}", next_prime(100)); // 101
+}
--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,21 +1,19 @@
-use std::cell::RefCell;
+mod messenger;
 use std::collections::HashMap;
+pub use crate::messenger::*;
+pub use std::cell::RefCell;
 
-pub mod messenger;
-
-pub use messenger::{Logger, Tracker};
-
-pub struct Worker {
-    pub track_value: std::rc::Rc<usize>,
+pub struct Worker{
+    pub track_value: Rc<usize>,
     pub mapped_messages: RefCell<HashMap<String, String>>,
     pub all_messages: RefCell<Vec<String>>,
 }
 
 impl Worker {
-    pub fn new(initial_value: usize) -> Self {
-        Worker {
-            track_value: std::rc::Rc::new(initial_value),
-            mapped_messages: RefCell::new(HashMap::new()),
+    pub fn new(value: usize) -> Worker {
+        Worker { 
+            track_value: Rc::new(value), 
+            mapped_messages: RefCell::new(HashMap::new()), 
             all_messages: RefCell::new(Vec::new()),
         }
     }
@@ -23,17 +21,17 @@ impl Worker {
 
 impl Logger for Worker {
     fn warning(&self, msg: &str) {
-        self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Warning: {}", msg));
-    }
-
-    fn info(&self, msg: &str) {
-        self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
+        self.all_messages.borrow_mut().push(String::from(msg));
+        self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg[9..].to_string());
     }
 
     fn error(&self, msg: &str) {
-        self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Error: {}", msg));
+        self.all_messages.borrow_mut().push(String::from(msg));
+        self.mapped_messages.borrow_mut().insert("Error".to_string(), msg[7..].to_string());
+    }
+
+    fn info(&self, msg: &str) {
+        self.all_messages.borrow_mut().push(String::from(msg));
+        self.mapped_messages.borrow_mut().insert("Info".to_string(), msg[6..].to_string());
     }
 }
